0a9686649effc62ead1f37e7e692820f2c53bbee
jaenster <github@jaenster.net>
2019-12-02 19:06:12 +0100

    refactor to separated function

--- client.js
+++ client.js
@@ -16,128 +16,115 @@ const net = require('net');
 
 class client {
 	constructor(socket) {
-		this.up = false;
-		this.status = 0;
 		this.socket = socket;
-
-		let cb;
-		socket.once('data', cb = data => {
-			try {
-				let size = 0;
-				switch (this.status) {
-					case 0: {
-						const version = data.readUInt8(0);
-						if (version !== 5) {
-							// Throw error, unsupported version
-							throw new Error('Unsupported version');
-						}
-						const typesSupported = data.readUInt8(1);
-
-						// build suported types
-						let auths = [];
-						for (let i = 0; i < typesSupported; i++) auths.push(data.readUInt8(i + 2));
-
-						// Filter out those we dont support. Currently hardcoded
-						auths = auths.filter(type => type === enums.AUTH || type === enums.NO_AUTH);
-
-						if (!auths.length) {
-							// Throw error, unsupported
-							throw new Error('Unsupported auth');
-						}
-
-						// The lowest of preferredAuthMethod's come first
-						auths.sort((a, b) => preferredAuthMethods.indexOf(a) - preferredAuthMethods.indexOf(b));
-
-						// What we want to auth with, comes first
-						const auth = auths[0];
-
-						const buffer = Buffer.alloc(2);
-						buffer.writeUInt8(0x05, size++); // version
-						buffer.writeUInt8(auth, size++); // Auth type
-
-						// Send response
-						// Reset is zero'd out
-						this.socket.write(buffer);
-						this.status++; // Next1
-					}
-						socket.once('data', cb);
-						break;
-					case 1: { // Waiting for username/password
-						size = 0;
-						let failed = 0x00; // didnt fail
-						const type = data.readUInt8(size++);
-						switch (type) {
-							case 0x01: // password / username
-
-								const usernameLength = data.readUInt8(size++); // ulength
-								size += usernameLength;
-								const passwordLength = data.readUInt8(size++); // ulength
-
-
-								const uname = BufferHelper.getString(data, usernameLength, 2);
-								const pass = BufferHelper.getString(data, passwordLength, size);
-
-								// ToDo; some check for username/password
-								// For now, we just accept
-								failed = 0x00; // we didnt
-								break;
-							default:
-								throw new Error('unsupported auth method');
-						}
-						const buffer = Buffer.alloc(2);
-						buffer.writeUInt8(type, 0); // username/password response
-						buffer.writeUInt8(failed, 1);
-
-						// ToDo; if failed, close connection
-						this.socket.write(buffer);
-						this.status++;
-						socket.once('data', cb);
-					}
-						break;
-					case 2: { // receiving ip/port
-						const command = data.readUInt8(++size);
-						size += 2; // we dont care for the reverse byte
-						const hostType = data.readUInt8(size++);
-
-						let ipAddr;
-						switch (hostType) {
-							case 0x01: // ip address
-								ipAddr = [size++, size++, size++, size++].map(offset => data.readUInt8(offset).toString()).join('.');
-								break;
-							case 0x03: // domain name
-							case 0x04: // IPV6
-						}
-						if (!ipAddr) {
-							throw new Error('Failed to get ip address');
-						}
-
-						const port = data.readUInt16BE(size);
-						if (!port) {
-							throw new Error('Failed to get port');
-						}
-
-						this.connect(ipAddr, port,data);
-					}
-				}
-			} catch (e) {
-				console.log(e.message);
-				console.log(e.stack)
-			}
-
-		});
-
-		client.instances.push(this);
+		socket.once('data', data => this.handshakeInit(data));
 	}
 
-	connect(ipAddr,port,connectBuffer) {
+	connect(ipAddr, port, connectBuffer) {
 		//ToDo; deal with failure's
 		const remote = net.connect(port, ipAddr, () => {
 			// Assuming always for now it is an success
-			connectBuffer.writeUInt8(0x00,1); // Success code
+			connectBuffer.writeUInt8(0x00, 1); // Success code
 			this.socket.write(connectBuffer);
 
 			new ProxyClient(this.socket, remote)
 		});
+	}
+
+	handshakeInit(data) {
+		let offset = 0;
+		const version = data.readUInt8(0);
+		if (version !== 5) {
+			// Throw error, unsupported version
+			throw new Error('Unsupported version');
+		}
+		const typesSupported = data.readUInt8(1);
+
+		// build suported types
+		let auths = [];
+		for (let i = 0; i < typesSupported; i++) auths.push(data.readUInt8(i + 2));
+
+		// Filter out those we dont support. Currently hardcoded
+		auths = auths.filter(type => type === enums.AUTH || type === enums.NO_AUTH);
+
+		if (!auths.length) {
+			// Throw error, unsupported
+			throw new Error('Unsupported auth');
+		}
+
+		// The lowest of preferredAuthMethod's come first
+		auths.sort((a, b) => preferredAuthMethods.indexOf(a) - preferredAuthMethods.indexOf(b));
+
+		// What we want to auth with, comes first
+		const auth = auths[0];
+
+		const buffer = Buffer.alloc(2);
+		buffer.writeUInt8(0x05, offset++); // version
+		buffer.writeUInt8(auth, offset++); // Auth type
+
+		// Send response
+		// Reset is zero'd out
+		this.socket.write(buffer);
+
+		// once handshake part 1 is done, wait for part 2.
+		this.socket.once('data', data => this.handshakeAuth(data));
+	}
+
+	handshakeAuth(data) { // Waiting for username/password
+		let offset = 0;
+		let failed = 0x00; // didnt fail
+		const type = data.readUInt8(offset++);
+		switch (type) {
+			case 0x01: // password / username
+
+				const usernameLength = data.readUInt8(offset++); // ulength
+				offset += usernameLength;
+				const passwordLength = data.readUInt8(offset++); // ulength
+
+
+				const uname = BufferHelper.getString(data, usernameLength, 2);
+				const pass = BufferHelper.getString(data, passwordLength, offset);
+
+				// ToDo; some check for username/password
+				// For now, we just accept
+				failed = 0x00; // we didnt
+				break;
+			default:
+				throw new Error('unsupported auth method');
+		}
+		const buffer = Buffer.alloc(2);
+		buffer.writeUInt8(type, 0); // username/password response
+		buffer.writeUInt8(failed, 1);
+
+		// ToDo; if failed, close connection
+		this.socket.write(buffer);
+		this.socket.once('data', data => this.handshakeConnect(data));
+	}
+
+	handshakeConnect(data) {
+		let offset = 0;
+		const command = data.readUInt8(++offset);
+		offset += 2; // we dont care for the reverse byte
+		const hostType = data.readUInt8(offset++);
+
+		let ipAddr;
+		switch (hostType) {
+			case 0x01: // ip address
+				ipAddr = [offset++, offset++, offset++, offset++].map(offset => data.readUInt8(offset).toString()).join('.');
+				break;
+			case 0x03: // domain name
+			case 0x04: // IPV6
+		}
+		if (!ipAddr) {
+			throw new Error('Failed to get ip address');
+		}
+
+		const port = data.readUInt16BE(offset);
+		if (!port) {
+			throw new Error('Failed to get port');
+		}
+
+		this.connect(ipAddr, port, data);
 	}
 
 	static instances = [];
