2ae957eac88161a35eea1d0f0a6377ebdf28071c
jaenster <github@jaenster.net>
2019-12-26 22:59:00 +0100

    update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,24 +1,31 @@
 @echo off
 
-REM Begin ->  Trick to run as admin
+REM Begin -  Trick to run as admin
 set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-REM End ->    Trick to run as admin
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+REM End -    Trick to run as admin
 
-REM Should need
-echo Installing nodejs 13
-.\installTools\installnodejs.vbs
+REM Should need node?
+echo Installing nodejs 13 if needed
+where node.exe >nul 2>&1 && echo + skipping nodejs  || .\installTools\installnodejs.vbs
 
-echo Installing npm build tools...
-cmd /c npm install --global --production windows-build-tools
+.\installTools\resetvars.vbs
+call "%TEMP%\resetvars.bat"
 
-echo Installing node-gyp....
-cmd /c npm install -g node-gyp
+where yarn >nul 2>&1 && echo + skipping yarn || cmd /c npm install -g yarn
 
-echo Installing needed dependencies...
-npm install
-echo
-echo
+.\installTools\resetvars.vbs
+call "%TEMP%\resetvars.bat"
+
+echo Installing windows build tools...
+cmd /c yarn global add windows-build-tools node-gyp
+
+echo Completing instalation by setting up everything
+yarn
+
+echo.
+echo.
 echo Ready to start.
-echo
-echo
+echo.
+echo.
+pause
